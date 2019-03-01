aefc921a57cf4d6cf59f4592738a932cc481d3e6
dapmas <sampadmedda@gmail.com>
2019-03-01 11:21:56 +0530

    added feature user is typing

--- public/chat.js
+++ public/chat.js
@@ -6,18 +6,32 @@ var socket = io.connect("http://localhost:4000");
 var message = document.getElementById('message');
     handle = document.getElementById('handle');
     btn = document.getElementById('send-message');
-    messageWindow = document.getElementById('message-window');
+    messageWindow = document.getElementById('message-window'),
+    feedback = document.getElementById('feedback');
 
 
 
-// Events
+// client Events
 btn.addEventListener('click', function() {
   socket.emit('chat', {
     message: message.value,
     handle: handle.value
   });
+  message.value= "";
 });
 
+message.addEventListener('keypress', function() {
+  socket.emit('typing', handle.value);
+});
+
+
+// socket events
 socket.on('chat', function(data) {
-  messageWindow.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>'
+  // += means append to the existing whereas only = means overwrite
+  feedback.innerHTML = ""
+  messageWindow.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
+});
+
+socket.on('typing', function(data) {
+  feedback.innerHTML = '<p><em>' + data + ' is typing...</em></p>';
 });
