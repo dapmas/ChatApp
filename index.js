80ffa3d633877d78a02f6338fc7ccac00639c6d5
dapmas <sampadmedda@gmail.com>
2019-03-01 10:47:14 +0530

    test on local tunnel

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 var express = require('express');
+// var socket = require('socket.io');
+var socket = require('socket.io')
 
 // App setup
 var app = express();
-var server = app.listen(8080, function() {
-  console.log("Listening to requests on port 8080.");
+var server = app.listen(4000, function() {
+  console.log("Listening to requests on port 4000.");
 });
-
 
 // Static Files
 app.use(express.static('public'));
+
+
+// Socket setup
+var io = socket(server);
+
+io.on('connection', function(socket) {
+  socket.on('chat', function(data) {
+    io.sockets.emit('chat', data);
+  });
+});
